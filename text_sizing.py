c16442cf23b4e8aa9d11bb3d0154668557965082
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2025-08-22 10:46:12 +0100

    add cursor position report

--- text_sizing.py
+++ text_sizing.py
@@ -1,9 +1,17 @@
 # https://sw.kovidgoyal.net/kitty/text-sizing-protocol/
+import re
+import sys
+import termios
+import tty
+
+STDIN_FILENO = sys.stdin.fileno()
 
 MIN_SCALE = 1
 MAX_SCALE = 7
 
 MAX_BYTES = 4096
+
+RE_CURSOR_POSITION = re.compile(r"\x1b\[(?P<row>\d+);(?P<col>\d+)R")
 
 
 def quickstart() -> None:
@@ -41,15 +49,38 @@ def print_compact_superscript(text: str) -> None:
     print()
 
 
+def get_cursor_position() -> tuple[int, int]:
+    # Adapted from https://jwodder.github.io/kbits/posts/cursor-pos/
+    print("\x1b[6n", end="", flush=True)
+
+    resp = b""
+    while not resp.endswith(b"R"):
+        resp += sys.stdin.buffer.read(1)
+
+    resp_str = resp.decode("utf-8", "surrogateescape")
+    if match := RE_CURSOR_POSITION.fullmatch(resp_str):
+        return (int(match["row"]), int(match["col"]))
+    else:
+        raise ValueError(resp_str)
+
+
 def main() -> None:
-    print_simple_superscript("simple superscript")
-    print_compact_superscript("compact superscript")
+    # Put the terminal in 'cbreak mode' to allow reading terminal responses
+    orig_attrs = tty.setcbreak(STDIN_FILENO, termios.TCSANOW)
 
-    for scale in range(MIN_SCALE, MAX_SCALE + 1):
-        print_big(f"x{scale}", scale)
+    print(get_cursor_position())
 
-    for scale in range(MIN_SCALE, MAX_SCALE + 1):
-        print_big(f"x{scale}", scale, newline=False)
+    # print_simple_superscript("simple superscript")
+    # print_compact_superscript("compact superscript")
+    #
+    # for scale in range(MIN_SCALE, MAX_SCALE + 1):
+    #     print_big(f"x{scale}", scale)
+    #
+    # for scale in range(MIN_SCALE, MAX_SCALE + 1):
+    #     print_big(f"x{scale}", scale, newline=False)
+
+    # Restore the original terminal settings
+    termios.tcsetattr(STDIN_FILENO, termios.TCSANOW, orig_attrs)
 
 
 if __name__ == "__main__":
